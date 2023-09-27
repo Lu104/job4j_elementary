252e7b30e1d91f60703c83ccc92ca95ad4a5e742
LUDMILA <oparina_boss@mail.ru>
2023-09-27 16:05:37 +0700

    4.3.5. Множественное логическое выражение И [#9461]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,14 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
-        return result;
-    }
-
-    public static int summation(int left, int right) {
-        boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
