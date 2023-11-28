3d5dade5179eaebfd48e9eb1bf73cfe2db0d6e3b
LUDMILA <oparina_boss@mail.ru>
2023-11-28 23:56:51 +0700

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,18 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         Float[] prices = new Float[40];
+        names[0] = "Petr Ivanov";
+        names[1] = "Ivan Petrov";
+        names[2] = "Egor Sidorov";
+        names[3] = "Pavel Pavlov";
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
